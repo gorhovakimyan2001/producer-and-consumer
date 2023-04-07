c09791f04cd4e808e97c547c798ed0915a399020
User <User@DESKTOP-67H0QUE>
2023-04-07 22:26:21 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿namespace Program
+{
+    class Program
+    {
+
+        private static Queue<int> stream = new Queue<int>();
+        private static Random random = new Random();
+        private static readonly Object o = new Object();
+        private static void produce()
+        {
+            int input;
+            lock(o)
+            {
+                while (true)
+                {
+                    if (stream.Count > 40)
+                        Monitor.PulseAll(o);
+                    if (stream.Count > 100)
+                    {
+                        Monitor.PulseAll(o);
+                        Monitor.Wait(o);
+                    }
+                    input = random.Next(1, 100);
+                    Console.WriteLine("PRODUCE");
+                    stream.Enqueue(input);
+                }
+            }
+        }
+
+        private static void consume()
+        {
+            lock (o)
+            {
+                while (true)
+                {
+                    Console.WriteLine("CONSUME");
+                    if (stream.Count == 0)
+                    {
+                        Monitor.PulseAll(o);
+                        Monitor.Wait(o);
+                    }
+                    
+                    if (stream.Count < 30)
+                    Monitor.Pulse(o);
+                    stream.Dequeue();
+                }
+                
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Thread t1 = new Thread(produce);
+            Thread t2 = new Thread(consume);
+
+            t1.Start();
+            t2.Start();
+
+            t1.Join();
+            t2.Join();
+
+            
+        }
+    }
+}
